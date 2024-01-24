f5d5ee4d6306f0746d81d559b0c09c190da8b7af
Dominik Gubrynowicz <56269092+Dominik-Gubrynowicz@users.noreply.github.com>
2024-01-24 21:38:48 +0100

    animate menu

--- menu.js
+++ menu.js
@@ -5,13 +5,15 @@ var menuButton = document.getElementById("menuButton");
 
 // Define a function to toggle the menu visibility
 function toggleMenu() {
-  // If the menu is hidden, show it
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-  } else {
-    // Otherwise, hide it
-    menu.style.display = "none";
-  }
+    // If the menu is hidden, show it and animate the button
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+      menuButton.classList.add("animate");
+    } else {
+      // Otherwise, hide it and remove the animation
+      menu.style.display = "none";
+      menuButton.classList.remove("animate");
+    }
 }
 
 // Add a click event listener to the menu button
